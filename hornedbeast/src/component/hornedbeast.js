09d9fc5929463be0cde937edd3c75e9a0530ec9e
Rawnaqaburumman <engrawnaqaburummn@gmail.com>
2021-09-04 01:39:52 +0300

    final

--- hornedbeast/src/component/hornedbeast.js
+++ hornedbeast/src/component/hornedbeast.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
 
 class Hornedbeast extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { favaretCounter: 0 }
+      
+    
 
+     
+    }
+
+    increasingCounter = () => {
+        this.setState({
+            favaretCounter:this.state.favaretCounter+1
+        })
+      
+    }
     render(){
 
        
@@ -9,11 +24,38 @@ class Hornedbeast extends React.Component{
          
                 
             return (
+
                 <div>
-                <h2>{this.props.titel}</h2>
-                <p>{this.props.description}</p>
-                <img src={this.props.imageUrl}alt={this.props.titel} titel={this.props.titel} style={{height: "300px" ,width:"300px"}}/>
-                </div>
+                <Card >
+                  <Card.Img  variant="top" onClick = {this.increasingCounter}  src={this.props.image_url}alt={this.props.titel}  style={{height: "300px" ,width:"300px"}}/>
+                  <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Text>
+                    {this.props.description}
+                    </Card.Text>
+                    
+                    <Card.Text>
+                    {this.state.favaretCounter} ❤️
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+               
+        
+              </div>
+
+
+
+
+
+
+            //     <div>
+            //     <h2>{this.props.titel}</h2>
+            //     <p>{this.props.description}</p>
+            //     <img onClick = {this.increasingCounter}  src={this.props.image_url}alt={this.props.titel} titel={this.props.titel} style={{height: "300px" ,width:"300px"}}/>
+            //   <h3> {this.state.favaretCounter} ❤️
+            //   </h3>
+            //     </div>
+               
             )
     }
 }
--- hornedbeast/src/component/main.js
+++ hornedbeast/src/component/main.js
@@ -1,28 +1,29 @@
 import React from "react";
-
+import data from '../assests/data';
 import Hornedbeast from './hornedbeast'
 class Main extends React.Component{
     render(){
 
-        return(
-          <main>
+      return (
+        <div>
+        {data.map((arrayOfData)=>{
 
-  
-            <Hornedbeast
-          title="UniWhal"
-          imageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
-          description="A unicorn and a narwhal nuzzling their horns"
+        
+         return (
+      
+              <Hornedbeast title = {arrayOfData.title} 
+              description = {arrayOfData.description}
+              image_url = {arrayOfData.image_url}
+              keyword ={arrayOfData.keyword}
+              horns = {arrayOfData.horns}
+          
+            
         />
-
-        <Hornedbeast
-
-        imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
-        description="Mother (or father) rhino with two babies"
-        title='Rhino Family'
-      />
-
-</main>
         )
-        }}
+      })}
+      
+      </div>
+          );
+      }} 
 
 export default Main;
